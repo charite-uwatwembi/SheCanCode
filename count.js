34417d8b85711d52fdf3f9fdeb40f77640e35a52
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-02-14 10:10:56 +0200

    shecancode

---
+++ count.js
@@ -0,0 +1,8 @@
+//Q: Count the occurrences of each word in the sentence.
+let sentence = " i love javascript, javascript is awesome!";
+let words = sentence.split(" ").reduce((acc, word) => {
+    acc[word] = (acc[word] || 0) + 1;
+    return acc;
+
+},{});
+console.log(words);//{i: 1, love: 1, javascript
